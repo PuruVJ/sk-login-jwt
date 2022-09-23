ca7f950cad5396f1134646d30091e08cb4ee7c27
Puru Vijay <devpuruvj@gmail.com>
2022-09-24 02:05:56 +0530

    Use ssl

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 
-const sql = postgres(env.DATABASE_URL);
+const sql = postgres(env.DATABASE_URL, { ssl: !dev });
 
 sql`create table if not exists users (
 	id serial primary key,
