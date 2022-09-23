dfa2606daaef83a6ef2e02f618f7a2e3726343e7
Puru Vijay <devpuruvj@gmail.com>
2022-09-24 02:03:35 +0530

    Push fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,4 +3,11 @@ import { env } from '$env/dynamic/private';
 
 const sql = postgres(env.DATABASE_URL);
 
+sql`create table if not exists users (
+	id serial primary key,
+	username varchar(255) not null,
+	email varchar(255) not null,
+	password varchar(255) not null
+)`;
+
 export { sql };
